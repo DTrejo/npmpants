3d6e98f03bd7adb5f3bf379db839075ee656d7cd
David Trejo <david.daniel.trejo@gmail.com>
2011-08-26 18:33:07 -0700

    stub project files from github.com/dtrejo/royalty project skeleton

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-// server.js
-var http = require('http')
-  , nko = require('nko')('fxFY6qeBj18FyrA2');
+var express = require('express')
+  , app = express.createServer()
+  , colors = require('colors')
+  , fs = require('fs')
+  , nko = require('nko')('fxFY6qeBj18FyrA2')
+  , PORT = parseInt(process.env.PORT, 10) || 8000
+  ;
 
-var app = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+// match app routes before serving static file of that name
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+//
+// These routes only work if a static file has not already been served.
+//
+app.get('/', function(req, res, next) {
   res.end('Hello, World');
 });
 
-app.listen(parseInt(process.env.PORT, 10) || 7777);
-console.log('Listening on ' + app.address().port);
+console.log('Your highness, at your service:'.yellow
+  + ' http://localhost:%d'.magenta, PORT);
+
+app.listen(PORT);
