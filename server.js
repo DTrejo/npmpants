9df60236c2aba88e17af1a1fb8a02ae9e55aa926
David Trejo <david.daniel.trejo@gmail.com>
2011-08-26 19:42:05 -0700

    test json data for modules page

--- server.js
+++ server.js
@@ -19,7 +19,26 @@ app.get('/modules/:name', function(req, res, next) {
 // returns json
 app.get('/api/modules/:name', function(req, res, next) {
   var name = req.params.name;
-  res.send({ some: 'json', name: name });
+
+  // test data via http://www.cpantesters.org/distro/A/a2pdf.json
+  res.send({
+    ostext: "GNU/Linux"
+  , status: "PASS"
+  , osversion: "2.6.35-28-generic"
+  , state: "pass"
+  , osname: "linux"
+  // , guid: "1484db3e-cee7-11e0-b293-00fe369c8ecd"
+  // , id: "15127442"
+  , platform: "x86_64-linux"
+  , version: "1.13"
+  , dist: "a2pdf"
+  , csspatch: "unp"
+  , distribution: "a2pdf"
+  , perl: "5.8.9"
+  , distversion: "a2pdf-1.13"
+  , cssperl: "rel"
+  , date: "201108250654"
+  });
 });
 
 console.log('Your highness, at your service:'.yellow
