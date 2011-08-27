a9ec0a38e97c2374ec5f1a9a0e8dde400dcd82c9
David Trejo <david.daniel.trejo@gmail.com>
2011-08-26 22:31:52 -0700

    make the box pop, also make the button work!

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  console.log('im ready');
+  var input = $('#input')
+    , button = $('#button');
+
+  input.bind('keypress', function(e) {
+    if (e.keyCode === 13) {
+      button.click();
+      e.preventDefault();
+      return false;
+    }
+  });
+  button.click(function(e) {
+    var module = escape($.trim(input.val())) || 'express';
+    window.location = '/modules/#' + module;
+    e.preventDefault();
+    return false;
+  });
 });
