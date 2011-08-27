67c55ca93c255fc0abedbaf1cb1f173e9829441d
Morgan "ARR!" Allen <morganrallen@gmail.com>
2011-08-27 00:47:22 -0700

    fixed execution path and test output

--- slave/lib/runner.js
+++ slave/lib/runner.js
@@ -1,15 +1,19 @@
 var cp = require("child_process"),
 	events = require("events"),
+	path = require("path"),
 	util = require("util"),
 	
 	_ = require("underscore");
 
 module.exports = Runner;
 
-function Runner(cmd) {
+function Runner(cmd, run_path) {
 	events.EventEmitter.call(this);
 
-	var p = cp.spawn(cmd);
+	cmd = cmd.split(" ");
+
+	process.chdir(run_path);
+	var p = cp.spawn(cmd[0], cmd.splice(1));
 
 	p.stdout.on("data", _.bind(this.onOut, this));
 	p.stderr.on("data", _.bind(this.onErr, this));
@@ -20,14 +24,14 @@ util.inherits(Runner, events.EventEmitter);
 
 Runner.prototype = {
 	onErr: function(err) {
-
+		console.log("[TEST ERR] " + err);
 	},
 	
 	onExit: function(code, sig) {
-		console.log("Test finished");
+		console.log("test completed");
 	},
 
 	onOut: function(data) {
-
+		console.log("[TEST] " + data);
 	}
 };
--- slave/slave.js
+++ slave/slave.js
@@ -2,11 +2,23 @@ var fs = require("fs"),
 	npm = require("npm"),
 	spawn = require("child_process").spawn,
 	
-	Runner = require("./lib/runner");
+	Runner = require("./lib/runner"),
+	
+	util = require("util");
 
-var module = "test";
+var queue = [], ready = false;
 
 npm.load(function() {
+	ready = true;
+	queue.forEach(function(module) {
+		exports.run(module);
+	});
+});
+
+exports.run = function(module) {
+	if(!ready) {
+		return queue.push(module);
+	};
 	npm.commands.install(".", module, function(err, data) {
 		var module_path = "./node_modules/" + module;
 
@@ -18,6 +30,6 @@ npm.load(function() {
 			throw new Error("package needs to define scripts.test");
 		}
 		
-		var r = new Runner(package.scripts.test);
+		var r = new Runner(package.scripts.test, module_path);
 	});
-});
+}
