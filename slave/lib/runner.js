8671eb5da8aa4f4ab37a77599cbb302c0adb5848
Morgan "ARR!" Allen <morganrallen@gmail.com>
2011-08-27 16:32:18 -0700

    :retab

--- slave/lib/runner.js
+++ slave/lib/runner.js
@@ -1,9 +1,9 @@
 var cp = require("child_process"),
-	events = require("events"),
-	path = require("path"),
-	util = require("util"),
-	
-	_ = require("underscore");
+  events = require("events"),
+  path = require("path"),
+  util = require("util"),
+  
+  _ = require("underscore");
 
 module.exports = Runner;
 
@@ -12,79 +12,79 @@ function determineSuite(cmd) {
 
 
 function Runner(cmd, run_path) {
-	events.EventEmitter.call(this);
+  events.EventEmitter.call(this);
 
-	// if npm.load is done yet the Runner gets queued
-	// once load is done Runner.run will be called directly
-	// from the slave driver
-	if(cmd)
-		this.run(cmd, run_path);
+  // if npm.load is done yet the Runner gets queued
+  // once load is done Runner.run will be called directly
+  // from the slave driver
+  if(cmd)
+    this.run(cmd, run_path);
 }
 
 util.inherits(Runner, events.EventEmitter);
 
 var RunnerPrototype = {
-	run: function(cmd, run_path) {
-		this.commandLine = {
-			args: [],
-			cmd: "",
-			envs: {}
-		};
+  run: function(cmd, run_path) {
+    this.commandLine = {
+      args: [],
+      cmd: "",
+      envs: {}
+    };
 
-		// split the command apart, cmd[0] will be the executable 
-		cmd = cmd.split(" ");
-		this.processCmdLine(cmd);
+    // split the command apart, cmd[0] will be the executable 
+    cmd = cmd.split(" ");
+    this.processCmdLine(cmd);
 
-		// are we using a suite? or a generic
-		determineSuite(cmd);
+    // are we using a suite? or a generic
+    determineSuite(cmd);
 
-		// run the command and pass everything else from the split as args
-		// (expresso ./tests, node test/test.js)
-		var p = cp.spawn(this.commandLine.cmd, this.commandLine.args, {
-			cwd: run_path,
-			env: _.extend(process.env, this.commandLine.envs)
-		});
+    // run the command and pass everything else from the split as args
+    // (expresso ./tests, node test/test.js)
+    var p = cp.spawn(this.commandLine.cmd, this.commandLine.args, {
+      cwd: run_path,
+      env: _.extend(process.env, this.commandLine.envs)
+    });
 
-		p.stdout.on("data", _.bind(this.onOut, this));
-		p.stderr.on("data", _.bind(this.onErr, this));
-		p.on("exit", _.bind(this.onExit, this));
-	},
-	processCmdLine: function(cmd) {
-		var env;
-		while(cmd[0] && cmd[0].indexOf("=") > -1) {
-			env = cmd.shift().split("=");
-			this.commandLine.envs[env[0]] = env[1];
-		}
+    p.stdout.on("data", _.bind(this.onOut, this));
+    p.stderr.on("data", _.bind(this.onErr, this));
+    p.on("exit", _.bind(this.onExit, this));
+  },
+  processCmdLine: function(cmd) {
+    var env;
+    while(cmd[0] && cmd[0].indexOf("=") > -1) {
+      env = cmd.shift().split("=");
+      this.commandLine.envs[env[0]] = env[1];
+    }
 
-		console.log("Determining test suite: " + cmd[0]);
+    console.log("Determining test suite: " + cmd[0]);
 
-		// TODO
-		// check cmd[0] for = and set approtriate env variables
+    // TODO
+    // check cmd[0] for = and set approtriate env variables
 
-		// this will prevent the need for global install of a test suite
-		// hopefully helping once we are overloading the suite to grab results
-		if(cmd[0] && cmd[0].indexOf("expresso") > -1) {
-			this.commandLine.cmd = path.join(process.cwd(), "/node_modules/expresso/bin/expresso");
-		} else {
-			this.commandLine.cmd = cmd[0];
-		}
+    // this will prevent the need for global install of a test suite
+    // hopefully helping once we are overloading the suite to grab results
+    if(cmd[0] && cmd[0].indexOf("expresso") > -1) {
+      this.commandLine.cmd = path.join(process.cwd(), "/node_modules/expresso/bin/expresso");
+    } else {
+      this.commandLine.cmd = cmd[0];
+    }
 
-		this.commandLine.args = cmd.slice(1);
+    this.commandLine.args = cmd.slice(1);
 
-		console.log(this.commandLine);
-	},
-	onErr: function(err) {
-		console.log("[TEST ERR] " + err);
-		this.emit("error", [err]);
-	},
-	
-	onExit: function(code, sig) {
-		this.emit("complete", code, sig);
-	},
+    console.log(this.commandLine);
+  },
+  onErr: function(err) {
+    console.log("[TEST ERR] " + err);
+    this.emit("error", [err]);      
+  },
+  
+  onExit: function(code, sig) {
+    this.emit("complete", code, sig);
+  },
 
-	onOut: function(data) {
-		console.log("[TEST] " + data);
-	}
+  onOut: function(data) {
+    console.log("[TEST] " + data);
+  }
 };
 
 _.extend(Runner.prototype, RunnerPrototype);
