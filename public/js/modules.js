172245079eae3dc3877e68b41e9ca59574a4f1cf
David Trejo <david.daniel.trejo@gmail.com>
2011-08-27 12:14:56 -0700

    modules page now shows link to github, if found

--- public/js/modules.js
+++ public/js/modules.js
@@ -1,3 +1,4 @@
+// runs on /modules/
 $(document).ready(function() {
 
   // need to add the hashchange plugin or whatnot.
@@ -6,12 +7,17 @@ $(document).ready(function() {
     if (status === 'success') {
       $('#name').text(data.name);
       $('#description').text(data.description);
-      $('#repo')
-        .text(data.name)
-        .attr('href', data.repository.url); // git clone URL, so deceptive. Fix!
+      if (data.repository.github) {
+        $('#repo')
+          .attr('href', data.repository.github)
+          .text(data.name);
+      } else {
+        $('#repo').parent().text('(Github URL not found for this package)');
+      }
       $('#author').text(data.author);
+
     } else {
-      // say sorry, shit's broken.
+      $('#description').text('Sorry, the request to the server failed.');
     }
   };
 });
