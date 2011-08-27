bc60051e11310705169e7dffe074c6d915ac3d16
Morgan "ARR!" Allen <morganrallen@gmail.com>
2011-08-27 11:25:01 -0700

    start of suite determination and code commenting

--- slave/lib/runner.js
+++ slave/lib/runner.js
@@ -7,9 +7,23 @@ var cp = require("child_process"),
 
 module.exports = Runner;
 
+function determineSuite(cmd) {
+	console.log("Determining test suite: " + cmd[0]);
+
+	// this will prevent the need for global install of a test suite
+	// hopefully helping once we are overloading the suite to grab results
+	if(cmd[0].indexOf("expresso") > -1) {
+		cmd[0] = path.join(process.cwd(), "/node_modules/expresso/bin/expresso");
+	}
+}
+
+
 function Runner(cmd, run_path) {
 	events.EventEmitter.call(this);
 
+	// if npm.load is done yet the Runner gets queued
+	// once load is done Runner.run will be called directly
+	// from the slave driver
 	if(cmd)
 		this.run(cmd, run_path);
 }
@@ -18,11 +32,20 @@ util.inherits(Runner, events.EventEmitter);
 
 var RunnerPrototype = {
 	run: function(cmd, run_path) {
+		// split the command apart, cmd[0] will be the executable 
 		cmd = cmd.split(" ");
 
+		// are we using a suite? or a generic
+		determineSuite(cmd);
+
+		// this doesn't work correctly and I suspect it will break
+		// once we have more then once suite running
+		// the runner needs to be forked off by the slave driver
 		if(run_path)
 			process.chdir(run_path);
 
+		// run the command and pass everything else from the split as args
+		// (expresso ./tests, node test/test.js)
 		var p = cp.spawn(cmd[0], cmd.splice(1));
 
 		p.stdout.on("data", _.bind(this.onOut, this));
--- slave/slave.js
+++ slave/slave.js
@@ -8,32 +8,45 @@ var fs = require("fs"),
 
 var queue = [], ready = false;
 
+// load needs to be call before any npm.commands can be run
+// but run needs to be call externally so we cannot do install from with in load
 npm.load(function() {
+	// tell slaveRunner.run we're ready
 	ready = true;
+	// then run out the que
 	queue.forEach(function(module) {
 		exports.run(module[0], module[1]);
 	});
 });
 
 exports.run = function(module, runner) {
+	// create out runner even if npm isn't ready
 	var r = runner || new Runner;
 
 	if(!ready) {
+		// we're not ready so add the module and the new runner to the que
 		queue.push([module, r]);
+		// return the runner so other components can subscribe to completed events
 		return r;
 	};
 
+	// ok, npm must be ready now, continue with the install
+	// install(here, module_name, cb);
 	npm.commands.install(".", module, function(err, data) {
+		// all modules are installed locally to prevent external problems
 		var module_path = "./node_modules/" + module;
 
+		// load the modules package.json
 		var package = JSON.parse(
 			fs.readFileSync(module_path + "/package.json").toString()
 		);
 		
+		// we only care about modules that provide a test in package.json
 		if(!(package.scripts && package.scripts.test)) {
 			throw new Error("package needs to define scripts.test");
 		}
 		
+		// tell the runner to go to work
 		r.run(package.scripts.test, module_path);
 	});
 
