85a092cec203062929a83177246b7f6cf6ba1bd3
ericz <really.ez@gmail.com>
2011-08-27 22:24:09 -0700

    move now functions outside of document ready

--- public/js/index.js
+++ public/js/index.js
@@ -36,34 +36,36 @@ $(document).ready(function() {
   }
 
 
-  // Load realtime data
-  now.testUpdated = function(data) {
-    if (console) {
-      console.log("Updated", data);
-    }
-  };
-  
-  // Add to list of recently updated repos
-  now.addToRecent = function(moduleArr) {
-    $(".recentitem").each(function(i) {
-      if(i > (9-moduleArr.length) ) {
-        $(this).remove();
-      }
-    });
-    for(var i = 0, ii = moduleArr.length; i < ii; i++) {
-      $("#recentbar").prepend($("<a></a>").addClass('recentitem').text(moduleArr[i]).prop('href', '/modules/#' + moduleArr[i]));
-    }
-  };
-  
-  // Add to list of recently updated tests
-  now.addToRecentTests = function(moduleArr) {
-    $(".recenttestitem").each(function(i) {
-      if(i > (9-moduleArr.length) ) {
-        $(this).remove();
-      }
-    });
-    for(var i = 0, ii = moduleArr.length; i < ii; i++) {
-      $("#recenttestbar").prepend($("<a></a>").addClass('recenttestitem').text(moduleArr[i]).prop('href', '/modules/#' + moduleArr[i]));
-    }
-  };
 });
+
+
+// Load realtime data
+now.testUpdated = function(data) {
+  if (console) {
+    console.log("Updated", data);
+  }
+};
+
+// Add to list of recently updated repos
+now.addToRecent = function(moduleArr) {
+  $(".recentitem").each(function(i) {
+    if(i > (9-moduleArr.length) ) {
+      $(this).remove();
+    }
+  });
+  for(var i = 0, ii = moduleArr.length; i < ii; i++) {
+    $("#recentbar").prepend($("<a></a>").addClass('recentitem').text(moduleArr[i]).prop('href', '/modules/#' + moduleArr[i]));
+  }
+};
+
+// Add to list of recently updated tests
+now.addToRecentTests = function(moduleArr) {
+  $(".recenttestitem").each(function(i) {
+    if(i > (9-moduleArr.length) ) {
+      $(this).remove();
+    }
+  });
+  for(var i = 0, ii = moduleArr.length; i < ii; i++) {
+    $("#recenttestbar").prepend($("<a></a>").addClass('recenttestitem').text(moduleArr[i]).prop('href', '/modules/#' + moduleArr[i]));
+  }
+};
