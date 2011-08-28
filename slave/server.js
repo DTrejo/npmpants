d76c8781d1ed7339f1c426365273ca914cbdeb09
Steve Wang <steve.john.wang@gmail.com>
2011-08-27 17:37:57 -0700

    Add comma to modules/index, add slave server (with crude REST API)

---
+++ slave/server.js
@@ -0,0 +1,14 @@
+var addToQueue;
+var server = require('http').createServer(function (req, res) {
+  if (req.url === '/favicon.ico') {
+    return;
+  }
+  addToQueue(req.url.substr(1));
+  res.writeHead({'Content-Type': 'text/html'});
+  res.end('');
+});
+server.listen(11235);
+
+addToQueue = function (name) {
+  require('slave').run(name);
+};
