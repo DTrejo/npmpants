6c6049d03fcaf92569ba9cde3df3051e925a12c5
Morgan "ARR!" Allen <morganrallen@gmail.com>
2011-09-24 09:29:01 -0700

    even more billboard layout/css tweaks

--- public/billboard/js/views/TestView.js
+++ public/billboard/js/views/TestView.js
@@ -3,14 +3,15 @@ BB.Views.TestView = BB.Views.BasicView.extend({
   tmpl: '\
       <div class="module-name"><%=id%></div> \
       <div class="module-results">\
-        <% mod=this;if(_.size(doc.tests) > 0) _.each(doc.tests, function(test, version){ %>\
+        <% mod=this;if(_.size(doc.tests) > 0) _(doc.tests).chain().sort().each(function(test, version){ %>\
           <div class="result-version">\
             <div class="module-version"><%= version !== "undefined" ? version : "NPM Install Failed" %></div>\
 			<div class="result-platforms">\
-				<% _.each(test, function(results, platform) { %>\
+				<% _(test).chain().keys().sort().each(function(k, v) { %>\
+				<% var results = test[k], platform = k; %>\
 				  <div class="result-platform">\
 					<div class="result-platform-name"><%=platform.split(" ")[0]%></div>\
-					<% _.each(results, function(result, nodeVersion) { %>\
+					<% _(results).chain().sort().each(function(result, nodeVersion) { %>\
 					  <div class="result-node-version <%=result.passed ? "greencheck":"redx"%>"><%=nodeVersion%></div>\
 					<% }); %>\
 				  </div>\
