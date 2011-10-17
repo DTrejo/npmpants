9766f9d0a83a950e64222397d5b60efa3c85c2a1
David Trejo <david.daniel.trejo@gmail.com>
2011-10-17 14:44:20 -0400

    CR: tap.js spaces to tabs, trim whitespace

--- slave/lib/handlers/tap.js
+++ slave/lib/handlers/tap.js
@@ -1,7 +1,8 @@
 var generic = require("./generic"),
-    cp = require("child_process"),
-    util = require("util"),
-    _ = require('underscore');
+	cp = require("child_process"),
+	path = require('path'),
+	util = require("util"),
+	_ = require('underscore');
 
 function TapHandler(cmd) {
 	this.name = "TapHandler";
@@ -13,37 +14,40 @@ util.inherits(TapHandler, generic);
 module.exports = TapHandler;
 
 TapHandler.prototype.run = function(workingDir) {
-  var env = _.extend(process.env, this.commandLine.envs);
+	var env = _.extend(process.env, this.commandLine.envs);
 
-  // console.log(this.commandLine.cmd, this.commandLine.args);
+	// console.log(this.commandLine.cmd, this.commandLine.args);
 
-  // last output format wins
-  //this.commandLine.args.push('--json');
+	// last output format wins
+	//this.commandLine.args.push('--json');
+	this.commandLine.cmd = path.join(
+		__dirname, "..", "..", "node_modules","tap","bin","tap.js"
+	);
 
-  var p = cp.spawn(this.commandLine.cmd, this.commandLine.args, {
-    cwd: workingDir,
-    env: env
-  });
+	var p = cp.spawn(this.commandLine.cmd, this.commandLine.args, {
+		cwd: workingDir,
+		env: env
+	});
 
-  p.stderr.on("data", _.bind(this.onErr, this));
-  p.stdout.on("data", _.bind(this.onStd, this));
-  p.on("exit", _.bind(this.onExit, this));
+	p.stderr.on("data", _.bind(this.onErr, this));
+	p.stdout.on("data", _.bind(this.onStd, this));
+	p.on("exit", _.bind(this.onExit, this));
 };
 
 TapHandler.prototype.output = '';
 TapHandler.prototype.name = 'TapHandler';
 // TapHandler.prototype.onStd = function (data) {
-//   this.output += data;
+//	 this.output += data;
 // }
-// 
+//
 // TapHandler.prototype.onErr = function(err, data) {
-//   console.log("error in TapHandler", err.toString(), data);
+//	 console.log("error in TapHandler", err.toString(), data);
 // };
-// 
+//
 // TapHandler.prototype.onExit = function(code, sig) {
-//   /*console.log('===');
-//   console.log(this.output); 
-//   console.log('===');*/
-//   console.log("complete", code <= 0, sig);
-//   this.emit("complete", code <= 0, sig);
+//	 /*console.log('===');
+//	 console.log(this.output);
+//	 console.log('===');*/
+//	 console.log("complete", code <= 0, sig);
+//	 this.emit("complete", code <= 0, sig);
 // };
