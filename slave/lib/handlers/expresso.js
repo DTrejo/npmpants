acd7b9d821dfb7d05078212271c6c85ea67ac56c
David Trejo <david.daniel.trejo@gmail.com>
2011-10-17 14:25:34 -0400

    CR: use local expresso install, no jscov

--- slave/lib/handlers/expresso.js
+++ slave/lib/handlers/expresso.js
@@ -1,7 +1,8 @@
 var generic = require("./generic"),
-    cp = require("child_process"),
-    util = require("util"),
-    _ = require('underscore');
+	cp = require("child_process"),
+	util = require("util"),
+	path = require('path'),
+	_ = require('underscore');
 
 function ExpressoHandler(cmd) {
 	this.name = "ExpressoHandler";
@@ -13,36 +14,37 @@ util.inherits(ExpressoHandler, generic);
 module.exports = ExpressoHandler;
 
 ExpressoHandler.prototype.run = function(workingDir) {
-  var env = _.extend(process.env, this.commandLine.envs);
+	var env = _.extend(process.env, this.commandLine.envs);
+	
+	// special path to a version of expresso without jscov, which does not compile
+	// on all architectures (I think only Solaris is broken).
+	this.commandLine.cmd = path.join(
+		__dirname, "..", "..", "expresso-noJScov","bin","expresso"
+	);
+	// console.log(this.commandLine.cmd, this.commandLine.args);
+	var p = cp.spawn(this.commandLine.cmd, this.commandLine.args, {
+		cwd: workingDir,
+		env: env
+	});
 
-  // console.log(this.commandLine.cmd, this.commandLine.args);
-
-  // last output format wins
-  //this.commandLine.args.push('--json');
-
-  var p = cp.spawn(this.commandLine.cmd, this.commandLine.args, {
-    cwd: workingDir,
-    env: env
-  });
-
-  p.stderr.on("data", _.bind(this.onErr, this));
-  p.stdout.on("data", _.bind(this.onStd, this));
-  p.on("exit", _.bind(this.onExit, this));
+	p.stderr.on("data", _.bind(this.onErr, this));
+	p.stdout.on("data", _.bind(this.onStd, this));
+	p.on("exit", _.bind(this.onExit, this));
 };
 
 ExpressoHandler.prototype.output = '';
 ExpressoHandler.prototype.onStd = function (data) {
-  this.output += data;
+	this.output += data;
 };
 
 ExpressoHandler.prototype.onErr = function(err, data) {
-  // console.log("error in ExpressoHandler", err.toString(), data);
+	// console.log("error in ExpressoHandler", err.toString(), data);
 };
 
 ExpressoHandler.prototype.onExit = function(code, sig) {
-  // console.log('===');
-  // console.log(this.output); 
-  // console.log('===');
-  // console.log("complete", code <= 0, sig);
-  this.emit("complete", code <= 0, sig);
+	// console.log('===');
+	// console.log(this.output); 
+	// console.log('===');
+	// console.log("complete", code <= 0, sig);
+	this.emit("complete", code <= 0, sig);
 };
