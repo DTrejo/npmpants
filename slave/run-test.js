203bf57b4ee7c05c5bc9325c477a9d6e6578ba4c
David Trejo <david.daniel.trejo@gmail.com>
2011-10-26 00:50:08 -0400

    slave: run-test.js uses new async slave-driver API

--- slave/run-test.js
+++ slave/run-test.js
@@ -3,23 +3,26 @@ var fs = require("fs"),
 require("colors");
 
 var module = process.argv[2] || "test";
-var s = slave.run(module);
 
-console.log("Running with NodeJS: " + process.version);
-var out = "", err = "";
+slave.ready(function(run) {
+	var s = run(module);
 
-s.on("out", function(data) {
-	out += data;
-	console.log("[test.js:out]:\n".green + data);
-});
+	console.log("Running with NodeJS: " + process.version);
+	var out = "", err = "";
 
-s.on("err", function(err, data) {
-	err += data;
-	console.log("[test.js:err]:\n".red + err, data);
-});
+	s.on("out", function(data) {
+		out += data;
+		console.log("[test.js:out]:\n".green + data);
+	});
 
-s.on("complete", function(code, sig) {
-	fs.writeFile("./logs/" + module + ".out.log", out);
-	fs.writeFile("./logs/" + module + ".err.log", err);
-	console.log("test completed with code:", code, 'sig:', sig);
+	s.on("err", function(err, data) {
+		err += data;
+		console.log("[test.js:err]:\n".red + err, data);
+	});
+
+	s.on("complete", function(code, sig) {
+		fs.writeFile("./logs/" + module + ".out.log", out);
+		fs.writeFile("./logs/" + module + ".err.log", err);
+		console.log("test completed with code:", code, 'sig:', sig);
+	});
 });
