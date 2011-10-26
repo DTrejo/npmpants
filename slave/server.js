add8d3e91accfa65870a135e842ea9fe239e3c94
David Trejo <david.daniel.trejo@gmail.com>
2011-10-26 00:52:17 -0400

    slave: server.js uses new async slave-driver API

--- slave/server.js
+++ slave/server.js
@@ -1,8 +1,8 @@
-var slaveDriver = require('./slave-driver');
-
-require('http').createServer(function (req, res) {
-	if (req.url !== '/favicon.ico') {
-		slaveDriver.run(req.url.substr(1));
-	}
-	res.end('');
-}).listen(11235);
+require('./slave-driver').ready(function(run) {
+	require('http').createServer(function (req, res) {
+		if (req.url !== '/favicon.ico') {
+			run(req.url.substr(1));
+		}
+		res.end('');
+	}).listen(11235);
+});
