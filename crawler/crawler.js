5cc98dc155f43de2bf5a88d43c8a319574392c97
David Trejo <david.daniel.trejo@gmail.com>
2011-11-03 01:10:50 -0400

    crawler: --range option for machines w/ little memory
    Also,
    - update usage updated in comment
    - use the production version of the tasks array

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -11,13 +11,15 @@ var args = require("argsparser").parse(),
 	testSuite = args["--suite"] || false,
 	uninstallAfter = args["--uninstall-after"] || false,
 	limit = args['--limit'] || false,
+	range = args['--range'] || false,
 	reportResults = args['--report-results'] || false,
 	speed = args['--series'] == true ? 'series' : 'parallel';
 
-/**
-	* Some magic function that takes the URL of a tarball. Should
-	* download, extract, run tests, and whatnot.
-	*/
+// most conservative usage:
+// 		./crawler/crawler.js --range --series --uninstall-after --report-results
+//
+// for testing this mode:
+// 		./crawler/crawler.js --range --series --uninstall-after --limit 10
 
 if (testSuite) {
 	console.log("YES --suite only running tests for " + testSuite);
@@ -28,9 +30,9 @@ if (reportResults) {
 	console.log('NOT --report-results to couchDB.');
 }
 if (uninstallAfter) {
-	console.log('YES --uninstall-after each package is tested');
+	console.log('YES --uninstall-after each package is tested (& cache clean)');
 } else {
-	console.log('NOT --uninstall-after each package is tested');
+	console.log('NOT --uninstall-after each package is tested (& cache clean)');
 }
 if (speed == 'series') {
 	console.log('YES --series tests run in <', speed, '>');
@@ -38,18 +40,62 @@ if (speed == 'series') {
 	console.log('NO --series tests run in <', speed, '>');
 }
 
-var parser = JSONStream.parse(['rows', /./]),
-	url = 'http://search.npmjs.org/api/_all_docs?include_docs=true';
-
+var URL = 'http://search.npmjs.org/api/_all_docs?include_docs=true';
 if (limit) {
-	url += '&limit=' + limit;
-	console.log('Limiting to', limit, 'modules.');
+	URL += '&limit=' + limit;
+	console.log('YES --limit to', limit, 'modules.');
 } else {
 	console.log('No --limit specified; fetching ALL docs from npm\'s couchdb!');
 }
+
+// let's get started.
 slave.ready(function(run) {
-	var req = request({ url: url }),
-		tasks = [];
+	if (range) {
+		console.log('YES --range, doing incremental ranges of packages until done');
+		runWithRange(run);
+	} else {
+		console.log('NO --range, starting at 0, going until limit or last package');
+		test(run);
+	}
+});
+
+function runWithRange(run) {
+	var numbers = '0123456789';
+	var letters = 'abcdefghijklmnopqrstuvwxyz';
+	var alphanumeric = (numbers + letters.toUpperCase() + letters).split('');
+
+	var tasks = alphanumeric.map(function(letter, i, list) {
+		var o = { start: letter, end: alphanumeric[i + 1] };
+		return async.apply(test, run, o);
+	});
+	// var tasks = [
+	// 	function (cb) { test(run, {start: '0', end: '1'}, cb); },
+	// 	function (cb) { test(run, {start: '7', end: '8'}, cb); },
+	// 	function (cb) { test(run, {start: 'z', end: undefined}, cb); }
+	// ];
+	async.series(tasks, function(err, results) {
+		if (err) console.log(err.stack);
+		console.log('vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv');
+		console.log('OFFICIALLY ALL DONE.');
+		console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+	});
+};
+
+function test(run, o, cb) {
+	var parser = JSONStream.parse(['rows', /./]);
+	if (o.start) {
+		// get the rest of things when on the 'z'
+		if (o.start !== 'z') o.end = '&endkey=' + encodeURIComponent('"'+o.end+'"');
+		else o.end = '';
+		o.start = '&startkey=' + encodeURIComponent('"'+o.start+'"');
+	} else {
+		o.start = '';
+		o.end = '';
+	}
+
+	var url = URL + o.start + o.end;
+	var req = request({ url: url });
+	var tasks = [];
 
 	parser.on('data', function(data) {
 		tasks.push(async.apply(processDoc, run, data));
@@ -57,15 +103,19 @@ slave.ready(function(run) {
 		// console.log('pushed', data.id);
 	});
 	parser.on('end', function() {
-		console.log('\ndone fetching data from search.npmjs.org');
+		console.log('\ndone fetching data from', url);
 		async[speed](tasks, function(err) {
 			if (err) console.log(err.stack);
-			console.log('done running tests!', tasks.length);
+			var text = [ o.start, o.end ].map(decodeURIComponent).join(' to ');
+			console.log('downloaded & tried to test', tasks.length,
+				'modules: [', text, ']');
+			console.log('==========================================================');
+			cb && cb();
 		});
 	});
 	console.log('Each dot stands for 10 modules queued for execution');
 	req.pipe(parser);
-});
+}
 
 function processDoc(run, el, cb) {
 	if (!el.id || !el.doc.versions) {
